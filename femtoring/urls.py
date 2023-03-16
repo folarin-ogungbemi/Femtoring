6918ffe9cbb7af0bcbcc981f0783603a6f83404d
Hasmelash95 <hermonasmelash@yahoo.com>
2023-03-16 13:46:03 +0000

    Add urls for home app and authentication app

--- femtoring/urls.py
+++ femtoring/urls.py
@@ -18,4 +18,6 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home.urls'), name='home_urls'),
+    path('accounts/', include('allauth.urls'))
 ]
