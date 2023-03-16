b11fdda8c25fee81cabea75e76b7ecd7987ef64f
Folarin Ogungbemi <Folarine1@gmail.com>
2023-03-16 22:08:07 +0000

    creRender tors and about page links

--- home/views.py
+++ home/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 
 
 class HomePageView(TemplateView):
     """Render the homepage"""
     template_name = "index.html"
+
+
+class AboutView(TemplateView):
+    """Render the About us view page"""
+    template_name = "about.html"
+
+
+class MentorsView(TemplateView):
+    """Render the Mentors page"""
+    template_name = "mentors.html"
