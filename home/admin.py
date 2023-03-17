d0d2cf8e3a3762988deedbe2e04de8e3b43bc3de
Folarin Ogungbemi <Folarine1@gmail.com>
2023-03-17 22:39:14 +0100

    Development branch flo (#42)
    * connect heroku hostname url
    
    * CSRF origins
    
    * Fix:Bug ','
    
    * Change database location
    
    * remove '*.' in CSRF trusted origins
    
    * Create User model
    
    * collect static settings
    
    * Modify auth settings
    
    * Modify models: remove rebase_type
    
    * Modify admin for users
    
    * Create forms.py file for users
    
    * Install Crispy forms and remove account verification

--- home/admin.py
+++ home/admin.py
@@ -1,8 +1,49 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from home.models import User, Booking, Mentor, Woman
 
 
-admin.site.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('id', 'username', 'email', 'type', 'is_staff', 'is_active',)
+    list_filter = ('type', 'is_staff', 'is_active',)
+    fieldsets = (
+        ('Type', {'fields': ('type',)}),
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name',)}),
+        ('Permissions', {'fields': (
+            'is_active', 'is_staff', 'is_superuser',
+            'groups', 'user_permissions')}),
+        ('Important Dates', {'fields': ('last_login', 'date_joined')}),)
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'username', 'email', 'password1', 'password2',
+                'type', 'is_staff', 'is_active')}),)
+    search_fields = ('username', 'email',)
+    ordering = ('id',)
+
+
+class WomanAdmin(UserAdmin):
+    model = Woman
+    list_display = ('id', 'username', 'email', 'type', 'is_staff', 'is_active',)
+    list_filter = ('is_staff', 'is_active',)
+    search_fields = ('username', 'email',)
+    ordering = ('id',)
+
+
+class MentorAdmin(UserAdmin):
+    model = Mentor
+    list_display = ('id', 'username', 'email', 'type', 'is_staff', 'is_active',)
+    list_filter = ('is_staff', 'is_active',)
+    search_fields = ('username', 'email',)
+    ordering = ('id',)
+
+
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(Woman, WomanAdmin)
+admin.site.register(Mentor, MentorAdmin)
 
 
 @admin.register(Mentor)
