6bf4665ca06eea4074d01666a5341115d5454750
Folarin Ogungbemi <Folarine1@gmail.com>
2023-03-17 18:01:15 +0000

    collect static settings

--- femtoring/settings.py
+++ femtoring/settings.py
@@ -152,9 +152,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'cloudinary_storage.storage.'\
-                      'StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
