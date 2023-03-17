a473ba034a551f081aad3e5b16d9f6d58dc47239
Hasmelash95 <103432143+Hasmelash95@users.noreply.github.com>
2023-03-17 22:57:03 +0000

    Create booking form model and new templates (#44)
    * Migrate auth account
    
    * Edit templates for account authentication
    
    * Change trusted origins url
    
    * Create booking model
    
    * Register the mentor model in django admin
    
    * Remove blank=True from time field
    
    * Add booking form model and booking views

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,8 @@ from home import views
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home_page'),
-    path('mentors/', views.MentorsView.as_view(), name='mentors_page'),
+    path('mentors/', views.MentorsList.as_view(), name='mentors_page'),
     path('about/', views.AboutView.as_view(), name='about_us_page'),
+    path('mentors/profile/', views.MentorDetail.as_view(), name="mentor_profile_page"),
+    path('mentors/booking/', views.BookingView.as_view(), name='booking_page')
 ]
