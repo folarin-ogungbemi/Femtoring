9537fc31ce14c163bc55c1d0142877e3dded42d7
Folarin Ogungbemi <Folarine1@gmail.com>
2023-03-17 12:55:00 +0000

    Fix:Bug ','

--- femtoring/settings.py
+++ femtoring/settings.py
@@ -37,9 +37,10 @@ else:
 CSRF_TRUSTED_ORIGINS = [
     'https://*.8000-folarinogungb-femtoring-cocwxzcxe0y.ws-eu90.gitpod.io/',
     'https://*.8000-folarinogungb-femtoring-3af5eilakby.ws-eu90.gitpod.io/',
-    'https://*.8000-folarinogungb-femtoring-fqeo2f18c6j.ws-eu90.gitpod.io/'
-    'https://*.femtoring-team2.herokuapp.com/'
+    'https://*.8000-folarinogungb-femtoring-fqeo2f18c6j.ws-eu90.gitpod.io/',
+    'https://*.femtoring-team2.herokuapp.com/',
     ]
+
 
 # Application definition
 
