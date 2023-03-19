ed02d6863e026f2885b091dfcef0da5cff3c27b7
Folarin Ogungbemi <Folarine1@gmail.com>
2023-03-19 01:47:07 +0000

    Resolve conflict

---
+++ static/js/base.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const caretDown = document.querySelector('.fa-caret-down');
+    const openTrashModal = document.querySelector('.open-trash-modal');
+    const closeMessage = document.querySelector('.close-message');
+    const deleteMessageModal = document.querySelector('.modal');
+    const userMessage = document.querySelector('.user-message');
+
+    const OpenMessageToggler = () =>{
+        userMessage.classList.toggle('is-active');
+    }
+    const ModalFunc= () =>{
+        deleteMessageModal.classList.add('is-active');
+    }
+
+    caretDown.addEventListener('click', () => OpenMessageToggler());
+    openTrashModal.addEventListener('click', () => ModalFunc());
+    closeMessage.addEventListener('click', () => {
+        deleteMessageModal.classList.remove('is-active');
+        userMessage.classList.remove('is-active');
+    });
+});
