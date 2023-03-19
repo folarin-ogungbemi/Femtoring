93c3184e808ffcdfe94867ac34e4f989f280de43
Folarin Ogungbemi <Folarine1@gmail.com>
2023-03-19 12:00:45 +0000

    Set user view condition based on authenticated user

--- home/views.py
+++ home/views.py
@@ -26,6 +26,7 @@ class MentorsList(ListView):
 class MentorDetail(DetailView):
     model = Mentor
     template_name = 'profile.html'
+    context_object_name = 'profile'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
