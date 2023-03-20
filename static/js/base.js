a9b385eed6a738f42abf03969d73fc04758a2163
Hasmelash95 <103432143+Hasmelash95@users.noreply.github.com>
2023-03-20 11:48:24 +0000

    Set a timer for message alerts (#88)

--- static/js/base.js
+++ static/js/base.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const closeMessage = document.querySelector('.close-message');
     const deleteMessageModal = document.querySelector('.modal');
     const userMessage = document.querySelector('.user-message');
+    const messageAlert = document.getElementById('message-alert-container')
 
     const OpenMessageToggler = () =>{
         userMessage.classList.toggle('is-active');
@@ -12,10 +13,17 @@ document.addEventListener("DOMContentLoaded", function() {
         deleteMessageModal.classList.add('is-active');
     }
 
+    if (messageAlert != undefined) {
+        setTimeout(function(){ 
+            messageAlert.classList.add('visually-hidden')
+         }, 4000);
+    }
+
     caretDown.addEventListener('click', () => OpenMessageToggler());
     openTrashModal.addEventListener('click', () => ModalFunc());
     closeMessage.addEventListener('click', () => {
         deleteMessageModal.classList.remove('is-active');
         userMessage.classList.remove('is-active');
     });
+
 });
